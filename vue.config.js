4157dd5368d5ecc21f6a50ed06e0e420511ba24f
mkurniaiqbal <mkurniaiqbal99@gmail.com>
2022-10-07 20:43:38 +0700

    Add Section Register

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  devServer: {
+    proxy: "http://159.223.57.121:8090/",
+  },
+});
